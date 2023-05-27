777ef1f87f0f04ac6f86ff7bc6b595865ff03b6c
pitmak <piter_makarov@mail.ru>
2023-05-27 20:59:09 +0500

    step 04

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/pitmak/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/pitmak/frontend-project-46/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/7c29ce8d5b4138cdfe65/maintainability)](https://codeclimate.com/github/pitmak/frontend-project-46/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/7c29ce8d5b4138cdfe65/test_coverage)](https://codeclimate.com/github/pitmak/frontend-project-46/test_coverage)
---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,17 @@
+import gendiff from '../src/gendiff.js';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test('compare JSON files', () => {
+  const expected = readFile('result');
+  const result = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(result).toBe(expected);
+});
