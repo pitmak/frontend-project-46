6c6a6841efa1877882191704b59efe38974cac6c
pitmak <piter_makarov@mail.ru>
2023-05-31 17:50:11 +0500

    refactoring

--- src/builddiff.js
+++ src/builddiff.js
@@ -4,46 +4,29 @@ const buildDiff = (obj1, obj2) => {
   const merged = { ...obj1, ...obj2 };
 
   return Object.keys(merged).sort().reduce((acc, key) => {
+    const newNode = { name: key };
+
     if (key in obj1 && key in obj2) {
       if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
-        return [...acc, {
-          name: key,
-          type: diffNodeType.recursed,
-          children: buildDiff(obj1[key], obj2[key]),
-        }];
+        newNode.type = diffNodeType.recursed;
+        newNode.children = buildDiff(obj1[key], obj2[key]);
+      } else if (obj1[key] == obj2[key]) {
+        newNode.type = diffNodeType.unchanged;
+        newNode.value = obj1[key];
+      } else {
+        newNode.type = diffNodeType.modified;
+        newNode.oldValue = obj1[key];
+        newNode.newValue = obj2[key];
       }
-
-      if (obj1[key] == obj2[key]) {
-        return [...acc, {
-          name: key,
-          type: diffNodeType.unchanged,
-          value: obj1[key],
-        }];
-      }
-
-      return [...acc, {
-        name: key,
-        type: diffNodeType.modified,
-        oldValue: obj1[key],
-        newValue: obj2[key],
-      }];
+    } else if (key in obj1) {
+      newNode.type = diffNodeType.deleted;
+      newNode.value = obj1[key];
+    } else if (key in obj2) {
+      newNode.type = diffNodeType.added;
+      newNode.value = obj2[key];
     }
 
-    if (key in obj1) {
-      return [...acc, {
-        name: key,
-        type: diffNodeType.deleted,
-        value: obj1[key],
-      }];
-    }
-
-    if (key in obj2) {
-      return [...acc, {
-        name: key,
-        type: diffNodeType.added,
-        value: obj2[key],
-      }];
-    }
+    return [...acc, newNode];
   }, []);
 };
 
